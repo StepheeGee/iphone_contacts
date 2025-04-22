aa89686eab516e60b2db3dfd9c418a46b360ebc1
Stephanie G Johnson <stephaniejohnson.esq@gmail.com>
2025-04-22 00:07:59 -0400

    added sitemap

--- iphone_contacts/settings.py
+++ iphone_contacts/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contacts',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
--- iphone_contacts/urls.py
+++ iphone_contacts/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from contacts.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('contacts.urls')),  
+    path('', include('contacts.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
+
 
